d7264c00b14f2b2ed6d7623d4dcf31e8712aa25c
notdistortion <johndoe56437@gmail.com>
2025-02-27 09:02:01 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,9 @@
-In progress
+--[[
+    R3TH PRIV
+
+    Will use a placeholder script until R3TH PRIV is finished.
+]]
+
+ODH_KEY = ""
+
+loadstring(game:HttpGet("https://api.overdrivehub.xyz/v1/auth"))()
